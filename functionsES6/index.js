6f037826ac93def008265796bc8554bdefdef6be
Richard Gill <richard@rgill.co.uk>
2017-06-28 09:54:13 +0100

    Removes hangover from node-seed

--- functionsES6/index.js
+++
@@ -1,7 +0,0 @@
-import * as functions from "firebase-functions"
-
-export let onProjectQuery = functions.database.ref('/projectQueries')
-  .onWrite((event) => {
-    const data = event.data.val()
-
-})
